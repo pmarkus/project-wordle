809d622b7cc4f7600f58e41f02074f69d3d0dd44
Markus Pettersson <pmarkus@users.noreply.github.com>
2024-03-15 14:22:50 +0100

    refactor individual guesses into its own component

--- src/components/GuessBoard/GuessBoard.js
+++ src/components/GuessBoard/GuessBoard.js
@@ -2,17 +2,23 @@ import React from "react";
 import { range } from "../../utils";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants.js";
 
+function Guess({ guess }) {
+  return (
+    <p className="guess">
+      {range(0, 5).map((i) => (
+        <span className="cell" key={i}>
+          {guess[i]}
+        </span>
+      ))}
+    </p>
+  );
+}
+
 function GuessBoard({ guesses }) {
   return (
     <div className="guess-results">
-      {range(0, NUM_OF_GUESSES_ALLOWED).map((y) => (
-        <p className="guess" key={y}>
-          {range(0, 5).map((x) => (
-            <span className="cell" key={x}>
-              {guesses.length > y && guesses[y].label[x]}
-            </span>
-          ))}
-        </p>
+      {range(0, NUM_OF_GUESSES_ALLOWED).map((i) => (
+        <Guess key={i} guess={guesses.length > i ? guesses[i].label : ""} />
       ))}
     </div>
   );
